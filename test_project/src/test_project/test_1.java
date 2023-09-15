9b53d640a2502a22113b5c726dca8c9e1c58063e
DevKimsunho <ksssk96@gmail.com>
2023-09-16 01:50:11 +0900

    commit test

--- test_project/src/test_project/test_1.java
+++ test_project/src/test_project/test_1.java
@@ -5,6 +5,8 @@ public class test_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		System.out.println("test Commit!");
+		
 	}
 
 }
