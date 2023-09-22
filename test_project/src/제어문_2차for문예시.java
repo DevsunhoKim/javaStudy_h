a4ecbaaab2894017c1b6e64374b166cb49630c0c
DevKimsunho <ksssk96@gmail.com>
2023-09-22 18:14:31 +0900

    2차 for 문

---
+++ test_project/src/제어문_2차for문예시.java
@@ -0,0 +1,22 @@
+
+public class 제어문_2차for문예시 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	
+	int ii=2, jj=1;
+
+	for (ii = 2; ii < 10; ii++) {
+		for (jj = 1; jj < 10; jj++ ) {
+			System.out.print(ii+" * "+jj+" = "+ii*jj+" \t");
+		}
+		System.out.println();
+	}
+
+}
+
+}
+
+// 2차 for문을 이용한 구구단과 유사한 출력물 생성
