880d03b8b1b8eb26eb938ce5e61ef589cb465dc2
DevKimsunho <ksssk96@gmail.com>
2023-09-23 17:44:52 +0900

    11

---
+++ test_project/src/test_project/별출력.java
@@ -0,0 +1,18 @@
+package test_project;
+
+public class 별출력 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			for(int i=1;i<=10;i++)
+			{
+				for(int j=1;j<=10;j++) 
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+	}
+
+}
