28971f4e9f1ef36e5393ee0a2cc5825403597c4c
DevKimsunho <ksssk96@gmail.com>
2023-09-24 20:12:54 +0900

    배열_예습

---
+++ test_project/src/test_project/배열_예습.java
@@ -0,0 +1,22 @@
+/*
+ * 배열이란
+ * 
+ * 같은 타입의 데이터를 연속된 공간에 나열
+ * 각 데이터에 인덱스를 부여해 놓는 자료구조
+ * 
+ */
+package test_project;
+
+public class 배열_예습 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 길이가 5인 int배열 선언
+		
+		int[] intArray = new int[5];
+		
+		int[] scores = new int[30];
+	}
+
+}
